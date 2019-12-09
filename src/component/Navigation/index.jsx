718e5de58d01a1a02485e6495103a3a6665442f2
Mlg29 <48533887+Mlg29@users.noreply.github.com>
2019-12-09 03:14:32 -0800

    Update index.jsx

--- src/component/Navigation/index.jsx
+++ src/component/Navigation/index.jsx
@@ -5,38 +5,38 @@ import { Link } from "react-router-dom"
 
 function Navigation() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light nav-bg">
+        <nav className="navbar navbar-expand-lg navbar-light nav-bg">
             <Link to={"/"} ><h1 className="nav-header"><span className="food">FOOD</span><span className="chef">CHEF</span></h1></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav"></div>
-            <div class="collapse navbar-collapse" id="navbarNav"></div>
-            <div class="collapse navbar-collapse" id="navbarNav"></div>
-            <div class="collapse navbar-collapse" id="navbarNav"></div>
+            <div className="collapse navbar-collapse" id="navbarNav"></div>
+            <div className="collapse navbar-collapse" id="navbarNav"></div>
+            <div className="collapse navbar-collapse" id="navbarNav"></div>
+            <div className="collapse navbar-collapse" id="navbarNav"></div>
             
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item pr-4 active">
-                <Link to="/Homepage" style={{color: "white"}} >HOME <span class="sr-only">(current)</span></Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item pr-4 active">
+                <Link to="/" style={{color: "white"}} >HOME <span class="sr-only">(current)</span></Link>
                 </li>
-                <li class="nav-item pr-4">
-                <Link t0="/About" style={{color: "white"}} >ABOUT</Link>
+                <li className="nav-item pr-4">
+                <Link to="/About" style={{color: "white"}} >ABOUT</Link>
                 </li>
-                <li class="nav-item pr-4">
-                <Link t0="/service" style={{color: "white"}} >SERVICES</Link>
+                <li className="nav-item pr-4">
+                <Link to="/service" style={{color: "white"}} >SERVICES</Link>
                 </li>
-                <li class="nav-item pr-4">
-                <Link t0="/team" style={{color: "white"}} >TEAM</Link>
+                <li className="nav-item pr-4">
+                <Link to="/team" style={{color: "white"}} >TEAM</Link>
                 </li>
-                <li class="nav-item pr-4">
+                <li className="nav-item pr-4">
                 <Link t0="/our-food" style={{color: "white"}} >OUR FOOD</Link>
                 </li>
-                <li class="nav-item pr-4">
-                <Link t0="/testimonial" style={{color: "white"}} >TESTIMONIALS</Link>
+                <li className="nav-item pr-4">
+                <Link to="/testimonial" style={{color: "white"}} >TESTIMONIALS</Link>
                 </li>
-                <li class="nav-item pr-4">
-                <Link t0="/contact" style={{color: "white"}} >CONTACT</Link>
+                <li className="nav-item pr-4">
+                <Link to="/contact" style={{color: "white"}} >CONTACT</Link>
                 </li>
                 
             </ul>
